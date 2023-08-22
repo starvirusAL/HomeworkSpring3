b5d84e01f42b0f40a3115a2cfef2f473a20f3516
starvirusAL <alexoid1990@gmail.com>
2023-08-22 08:47:33 +0300

    //Додав в клас AbstractEntity поля createdDate, lastModifiedDate.

--- src/main/java/app/models/AbstractEntity.java
+++ src/main/java/app/models/AbstractEntity.java
@@ -1,7 +1,23 @@
 package app.models;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
 public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
 
 
